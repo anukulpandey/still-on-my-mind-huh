78e8e787ea8d33289af78d81cc1fbd33107044fa
Anukul Pandey <anukul.030601@gmail.com>
2025-09-21 01:38:04 +0530

    feat: bold

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="w-full py-4 text-center text-gray-500 text-sm border-t border-gray-200 mt-auto">
-        Made with <span className="text-blue-500">💙</span> by {import.meta.env.VITE_FOOTER_TEXT}
+        Made with <span className="text-blue-500">💙</span> by <span className="font-bold">{import.meta.env.VITE_FOOTER_TEXT}</span>
       </footer>
     );
   }
